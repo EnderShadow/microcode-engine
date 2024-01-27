8dfcc2fcbe413655e73458c445d7166ef01e41d2
Matthew Warren <professional@matthewwarren.me>
2024-01-27 00:00:32 -0500

    Remove repetition for ! prefix

--- microcode-defs.asm
+++ microcode-defs.asm
@@ -104,29 +104,24 @@
     ; store => 0x11 ; skipped because of special argument handling
 }
 
-#subruledef instruction
+#subruledef maybe_final_instruction
 {
     movz{sz: size} {dst: register}, {src: register} => 0b0 @ sz @ 0x0 @ src @ dst
     movs{sz: size} {dst: register}, {src: register} => 0b1 @ sz @ 0x0 @ src @ dst
-    !movz{sz: size} {dst: register}, {src: register} => 0b0 @ sz @ 0x8 @ src @ dst
-    !movs{sz: size} {dst: register}, {src: register} => 0b1 @ sz @ 0x8 @ src @ dst
 
     ldcz{sz: size} {dst: register}, {imm: u8} => 0b0 @ sz @ 0x1 @ imm @ dst
     ldcs{sz: size} {dst: register}, {imm: i8} => 0b1 @ sz @ 0x1 @ imm @ dst
-    !ldcz{sz: size} {dst: register}, {imm: u8} => 0b0 @ sz @ 0x9 @ imm @ dst
-    !ldcs{sz: size} {dst: register}, {imm: i8} => 0b1 @ sz @ 0x9 @ imm @ dst
 
     {op: operation}{sz: size} {dst: register} => 0b0 @ sz @ 0x2 @ op @ dst
-    !{op: operation}{sz: size} {dst: register} => 0b0 @ sz @ 0xA @ op @ dst
     store{sz: size} => 0b0 @ sz @ 0x2 @ 0x11 @ 0xDF
-    !store{sz: size} => 0b0 @ sz @ 0xA @ 0x11 @ 0xDF
 
     read_port {port: u4}, {dst: register} => port @ 0x3 @ 0x00 @ dst
-    !read_port {port: u4}, {dst: register} => port @ 0xB @ 0x00 @ dst
 
     write_port {port: u4}, {dst: register} => port @ 0x4 @ src @ 0x00
-    !write_port {port: u4}, {src: register} => port @ 0xC @ src @ 0x00
+}
 
+#subruledef never_final_instruction
+{
     seg{j: jump_name} => j @ 0b101 @ 0x00 @ 0x00
 
     {j: jump_name} {target: register} => j @ 0b110 @ target @ 0x00
@@ -134,6 +129,14 @@
     {j: jump_name} {displacement: i16} => j @ 0b111 @ le(displacement)
     
     nop => 0x7F @ 0x00 @ 0x00
+}
+
+#subruledef instruction
+{
+    {instr: maybe_final_instruction} => instr
+    !{instr: maybe_final_instruction} => (instr | 0x80000)`24 ; no clue why it doesn't know it's 24 bits already
+
+    {instr: never_final_instruction} => instr
 }
 
 pad_to_32_bits = 0
