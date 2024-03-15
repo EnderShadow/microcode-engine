00b694e58958d85c9953e0f715b410b5bd4d86ce
Matthew Warren <professional@matthewwarren.me>
2024-03-15 13:00:48 -0400

    Add LEG

---
+++ leg.asm
@@ -0,0 +1,106 @@
+#include "microcode-defs.asm"
+
+#alias %sp                  = %r8
+
+#alias %r_src1              = %ri0
+#alias %r_src2              = %ri1
+#alias %r_arg1              = %ri2
+#alias %r_arg2              = %ri3
+#alias %r_dst               = %ri4
+
+#alias %r_imm1              = %i0
+#alias %r_imm2              = %i1
+#alias %r_imm3              = %i2
+#alias %r_minus_one         = %i3
+
+#const port_io              = 0
+#const port_ip              = 1
+
+;  !@*#-instruction
+
+nop_seg:
+    @   nop
+read_ip:
+    @   read_port port_ip, %r6
+read_input:
+    @   read_port port_io, %r7
+write_ip:
+        write_port port_ip, %r6
+   !    nop
+write_output:
+   !    write_port port_io, %r7
+add:
+        movzh %alu_b, %r_arg2
+    @   addh %r_dst, %r_arg1
+sub:
+        movzh %alu_b, %r_arg2
+    @   subh %r_dst, %r_arg1
+and:
+        movzh %alu_b, %r_arg2
+    @   andh %r_dst, %r_arg1
+or:
+        movzh %alu_b, %r_arg2
+    @   orh %r_dst, %r_arg1
+not:
+        movzh %alu_b, %r_minus_one
+    @   xorh %r_dst, %r_arg1
+xor:
+        movzh %alu_b, %r_arg2
+    @   xorh %r_dst, %r_arg1
+shl:
+        movzh %alu_b, %r_arg2
+    @   shlh %r_dst, %r_arg1
+shr:
+        movzh %alu_b, %r_arg2
+    @   lshrh %r_dst, %r_arg1
+load:
+    @   loadh %r_dst, %r_arg1
+store:
+        movzh %alu_b, %r_arg2
+   !    storeh %r_arg1
+push:
+        movzh %alu_b, %r_arg2
+        storeh %sp
+        ldczh %alu_b, 1
+   !    addh %sp, %sp
+pop:
+        ldczh %alu_b, 1
+        subh %sp, %sp
+    @   loadh %r_dst, %sp
+call:
+        read_port port_ip, %alu_b
+        write_port port_ip, %r_imm3
+        storeh %sp
+        ldczh %alu_b, 1
+   !    addh %sp, %sp
+ret:
+        ldczh %alu_b, 1
+        subh %sp, %sp
+        loadh %r6, %sp
+        write_port port_ip, %r6
+   !    nop
+jmp_prefix:
+        movzh %alu_b, %r_arg2
+    @   subh %r_null, %r_arg1
+jeq:
+        jeq jmp
+   !    nop
+jne:
+        jne jmp
+   !    nop
+jb:
+        jb jmp
+   !    nop
+jbe:
+        jbe jmp
+   !    nop
+ja:
+        ja jmp
+   !    nop
+jae:
+        jae jmp
+   !    nop
+jmp:
+        write_port port_ip, %r_imm3
+nop:
+   !    nop
