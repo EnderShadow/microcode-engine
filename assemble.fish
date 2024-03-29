e4688c87f64083de515c14071d08e6b5e66a8a17
Matthew Warren <professional@matthewwarren.me>
2024-03-29 14:27:33 -0400

    Update file extensions for assemble script

--- assemble.fish
+++ assemble.fish
@@ -5,12 +5,16 @@ or exit 1
 
 if test \( -z "$_flag_format" \) -o \( 'binary' = "$_flag_format" \)
     set format 'binary' '-dannotated=false'
+    set extension 'bin'
 else if test "$_flag_format" = 'symbols'
     set format 'symbols' '-dannotated=false'
+    set extension 'sym'
 else if test "$_flag_format" = 'tcgame'
     set format 'tcgame,group:8' '-dannotated=true'
+    set extension 'ann'
 else if test "$_flag_format" = 'annotated'
     set format 'annotated,group:8' '-dannotated=true'
+    set extension 'ann'
 else
     echo "Unsupported format: $_flag_format"
     echo 'Supported formats are: binary, tcgame, or annotated'
@@ -21,8 +25,11 @@ if test "$_flag_output" = '--'
     set output "--print"
 else if test -n "$_flag_output"
     set output "--output=$_flag_output"
+else if test (count $argv) = 1
+    set new_name (path change-extension $extension $argv)
+    set output "--output=$new_name"
 else
-    set output ""
+    set output "out.$extension"
 end
 
 set argc (count $argv)
@@ -34,10 +41,6 @@ for idx in (seq $argc)
     end
 end
 
-if test -n "$output"
-    customasm -f $format $output $argv
-else
-    customasm -f $format $argv
-end
+customasm -f $format $output $argv
 
 rm *.tmp
